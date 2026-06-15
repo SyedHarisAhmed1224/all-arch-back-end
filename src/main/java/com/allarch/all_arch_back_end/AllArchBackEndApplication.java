67911a49d4bcd03a827ea011444b4f7556b43683
Syed Haris Ahmed <syedharisahmed1224@gmail.com>
2026-06-15 22:51:10 +0500

    Initial Commit

---
+++ src/main/java/com/allarch/all_arch_back_end/AllArchBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.allarch.all_arch_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllArchBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllArchBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allarch/all_arch_back_end/AllArchBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allarch.all_arch_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllArchBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
